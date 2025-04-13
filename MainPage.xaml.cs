5a5832dd055a8c28a6914442ab9e84f00d642c0d
gefakra <61078526+gefakra@users.noreply.github.com>
2025-04-13 18:28:28 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private WeatherService _weatherService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _weatherService = new WeatherService();
+        }
+
+        private async void OnGetWeatherClicked(object sender, EventArgs e)
+        {
+            string city = CityEntry.Text;  // Вводим город
+            var weatherData = await _weatherService.GetWeatherAsync(city);
+
+            if (weatherData != null)
+            {
+                WeatherLabel.Text = $"{weatherData.Name}: {weatherData.Main.Temp}°C\n" +
+                                    $"Humidity: {weatherData.Main.Humidity}%";
+            }
+            else
+            {
+                WeatherLabel.Text = "Error fetching weather data.";
+            }
+        }
+    }
+
+}
---
+++ WeatherService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class WeatherService
+{
+    private const string ApiKey = "YOUR_API_KEY"; // Замените на свой API-ключ
+    private const string ApiUrl = "http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric";
+
+    public async Task<WeatherData> GetWeatherAsync(string city)
+    {
+        using (var client = new HttpClient())
+        {
+            var url = string.Format(ApiUrl, city, ApiKey);
+            var response = await client.GetStringAsync(url);
+            var weatherData = JsonConvert.DeserializeObject<WeatherData>(response);
+            return weatherData;
+        }
+    }
+}
+
+public class WeatherData
+{
+    public MainData Main { get; set; }
+    public string Name { get; set; }
+}
+
+public class MainData
+{
+    public float Temp { get; set; }
+    public float Humidity { get; set; }
+}
