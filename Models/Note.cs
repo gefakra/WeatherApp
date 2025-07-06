2237bfc2fe8c32f08457fe69ff5c09900522283b
gefakra <61078526+gefakra@users.noreply.github.com>
2025-07-06 12:45:11 +0300

    Add files via upload

---
+++ Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace WeatherApp.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string NoteText { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; }
+        public string Location { get; set; }
+        public string WeatherCondition { get; set; }
+    }
+}
